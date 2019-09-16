892660b27a28da5c4f6099e9ed8ab31402d20ef4
Maxwell Stone <maxwellstone@maxwells-mbp.lan>
2019-09-15 20:06:28 -0400

    latest on photo code

--- EditProfileScreen.js
+++ EditProfileScreen.js
@@ -4,6 +4,9 @@ import { Button } from 'react-native-elements'
 import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
 import AsyncStorage from '@react-native-community/async-storage'
 import Config from './config'
+import ImagePicker from 'react-native-image-picker'
+import RNHeicConverter from 'react-native-heic-converter'
+//import AWS from 'aws-sdk'
 
 export default class EditProfileScreen extends Component {
     constructor(props) {
@@ -11,16 +14,40 @@ export default class EditProfileScreen extends Component {
         this.state = {
             user: props.navigation.getParam('user', {}),
             width: Math.round(Dimensions.get('window').width),
-            linkedPeople: []
+            linkedPeople: [],
+            profileImage: [],
+            token: ''
         }
         this.linkedPeopleEle = this.linkedPeopleEle.bind(this)
+        this.userImage = this.userImage.bind(this)
+
     }
 
     static navigationOptions = ({ navigation }) => {
         return {
-            title: 'Edit Profile'
+            title: 'Edit Profile',
+            headerRight: (
+                <Button title="Save" onPress={navigation.getParam('Save')} buttonStyle={{ backgroundColor: '#ffffff' }} titleStyle={{ color: '#fe5f55' }} />
+            )
         }
     }
+
+    async load(key) {
+        try {
+            let dataJ = await AsyncStorage.getItem(key)
+            let data = JSON.parse(dataJ)
+            this.setState({
+                token: data.Token
+            })
+            this.signIn()
+            return "true"
+        } catch (error) {
+            console.log("load error: " + error)
+            return "false"
+        }
+    }
+
+
 
     async componentDidMount() {
         let user = this.state.user
@@ -37,7 +64,9 @@ export default class EditProfileScreen extends Component {
         await this.setState({
             linkedPeople: this.linkedPeopleEle()
         })
+        this.load('currentUser')
         console.log('linked: ' + this.state.linkedPeople)
+
     }
 
     async linkedPeopleEle() {
@@ -57,6 +86,104 @@ export default class EditProfileScreen extends Component {
             return true
         }
     }
+
+    async userImage() {
+        const options = {
+            title: 'Select Profile Image',
+            color: 'blue',
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+        };
+
+        /**
+         * The first arg is the options object for customization (it can also be null or omitted for default options),
+         * The second arg is the callback which sends object: response (more info in the API Reference)
+         */
+        ImagePicker.showImagePicker(options, async (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                const source = { uri: response.uri };
+
+                // You can also display the image using data:
+                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+                var image = this.state.user
+                image.profileImage = source
+                console.log('response data', response.data)
+
+
+
+                RNHeicConverter.convert({ path: this.state.user.userImage }).then((result) => {
+                    let user = this.state.user
+                    user.userImage = result.path
+                    this.setState({
+                        user: user
+                    })
+                })
+
+                this.setState({
+                    user: image,
+                    userImage: response
+                });
+
+                /*var s3 = new AWS.S3({ accessKeyId: 'AKIAZQ2OPAJ25DM23QK4', secretAccessKey: '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', region: 'USEast' })
+                var params = { Bucket: 'kidcrews', Key: 'user_images/', ContentType: 'image/jpeg' }
+                s3.getSignedUrl('putObject', params, function (err, url) {
+                    console.log('Your generated pre-signed ULR is', url)
+                })*/
+
+                let server = Config.server + "/api/users/image"
+                //let body = this.createFormData(this.state.userImage, {})
+                let body = {
+                    'photo': this.state.profileImage
+                }
+                console.log(body)
+                console.log("fetching")
+                fetch(server, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'image/jpg',
+                        'Token': 'Token ' + this.state.token
+                    },
+                    body: body
+                })
+                    .then((response) => response.text())
+                    .then(async (responseJSON) => {
+                        console.log(responseJSON)
+
+                    })
+                    .catch((error) => {
+                        alert("error")
+                        console.log("error: " + error + "; server: " + server + "; json: " + body)
+                    })
+            }
+        });
+    }
+
+    createFormData(photo, body) {
+        const data = new FormData();
+
+        data.append("photo", {
+            name: photo.fileName,
+            type: photo.type,
+            uri:
+                Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+        });
+
+        Object.keys(body).forEach(key => {
+            data.append(key, body[key]);
+        });
+
+        return data;
+    };
 
     render() {
 
@@ -80,7 +207,7 @@ export default class EditProfileScreen extends Component {
             <ScrollView>
                 <View style={{ flex: 1, alignItems: 'center', padding: 10, borderBottomColor: "#495867", borderBottomWidth: StyleSheet.hairlineWidth }}>
                     <Image source={this.state.user.profileImage} style={{ width: width * 0.3, height: width * 0.3, borderRadius: (width * 0.3) / 2, marginBottom: 10 }} />
-                    <TouchableOpacity style={{}}><Text style={{ color: "#fe5f55" }}>Change Profile Photo</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={this.userImage} style={{}}><Text style={{ color: "#fe5f55" }}>Change Profile Photo</Text></TouchableOpacity>
                 </View>
                 <View style={{ flex: 1, flexDirection: "row", padding: 5, borderBottomColor: "#495867", alignItems: 'stretch', justifyContent: 'center' }}>
                     <View style={{ marginLeft: 5, marginRight: 5, width: 80 }}>
@@ -111,7 +238,6 @@ export default class EditProfileScreen extends Component {
                     </View>
                 </View>
                 <View>
-                    {this.state.linkedPeople}
                 </View>
             </ScrollView>
         )
